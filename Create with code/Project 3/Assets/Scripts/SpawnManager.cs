5ea450a4331be3ca3147698a39e8cf92d7462e0b
Zack <zachary.o531@stu.nebo.edu>
2019-12-19 09:05:44 -0700

    Project 3

---
+++ Create with code/Project 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerController playeyControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playeyControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+    void SpawnObstacle ()
+    {
+        if (playeyControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+    
+}
