14701bdc033ea0d51081cb99b37e9285336cba51
Zack <zachary.o531@stu.nebo.edu>
2020-01-02 10:28:13 -0700

    Tried to get score working and some more code

--- Create with code/Prototype 5/Assets/Scripts/GameManager.cs
+++ Create with code/Prototype 5/Assets/Scripts/GameManager.cs
@@ -9,12 +9,15 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
         score = 0;
         scoreText.text = "Score: " + score;
+        UpdateScore(0);
     }
 
     IEnumerator SpawnTarget()
@@ -26,6 +29,19 @@ public class GameManager : MonoBehaviour
             Instantiate(targets[index]);
         }
     }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+
+
+
+
+
+
 
     // Update is called once per frame
     void Update()
--- Create with code/Prototype 5/Assets/Scripts/Target.cs
+++ Create with code/Prototype 5/Assets/Scripts/Target.cs
@@ -10,6 +10,8 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     Vector3 RandomForce()
@@ -43,6 +46,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(5);
     }
 
     private void OnTriggerEnter(Collider other)
